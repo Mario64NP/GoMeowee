33fcf8a8afb7d5edf7d5c4008ed780f78c312bf0
Mario64NP <mj20171055@student.fon.bg.ac.rs>
2025-12-29 10:29:02 +0100

    Add base VM

---
+++ GoMeowee/ViewModels/BaseViewModel.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace GoMeowee.ViewModels;
+
+public abstract class BaseViewModel : INotifyPropertyChanged
+{
+    public event PropertyChangedEventHandler? PropertyChanged;
+
+    protected void OnPropertyChanged([CallerMemberName] string? propertyName = null)
+    {
+        PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+    }
+
+    private bool _isBusy;
+    public bool IsBusy
+    {
+        get => _isBusy;
+        set
+        {
+            if (_isBusy == value)
+                return;
+
+            _isBusy = value;
+            OnPropertyChanged();
+        }
+    }
+}
